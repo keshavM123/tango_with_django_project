5b04d18581a184df2e030212ddab4e706e71f7fd
Keshav Malpani <2398218M@student.gla.ac.uk>
2021-02-04 00:06:22 +0000

    end of chapter 8

--- rango/views.py
+++ rango/views.py
@@ -30,7 +30,11 @@ def about(request):
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/about.html', context=context_dict)
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
